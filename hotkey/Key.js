cab12b4620dee8e15e1c402d331270c7bc7baf3e
leichao <842333731@qq.com>
2015-02-05 11:01:15 +0800

    hotkey

---
+++ hotkey/Key.js
@@ -0,0 +1,162 @@
+var Key = {
+	"9": "Tab",
+	"12": "Clear",
+	"13": "Enter",
+	"16": "Shift_L",
+	"17": "Control_L",
+	"18": "Alt_L",
+	"19": "Pause",
+	"20": "Capital",
+	"27": "Esc",
+	"32": "Space",
+	"33": "Prior",
+	"34": "Next",
+	"35": "End",
+	"36": "Home",
+	"37": "Left",
+	"38": "Up",
+	"39": "Right",
+	"40": "Down",
+	"41": "Select",
+	"42": "Print",
+	"43": "Execute",
+	"45": "Insert",
+	"46": "Delete",
+	"47": "Help",
+	"48": "0",
+	"49": "1",
+	"50": "2",
+	"51": "3",
+	"52": "4",
+	"53": "5",
+	"54": "6",
+	"55": "7",
+	"56": "8",
+	"57": "9",
+	"65": "A",
+	"66": "B",
+	"67": "C",
+	"68": "D",
+	"69": "E",
+	"70": "F",
+	"71": "G",
+	"72": "H",
+	"73": "I",
+	"74": "J",
+	"75": "K",
+	"76": "L",
+	"77": "M",
+	"78": "N",
+	"79": "O",
+	"80": "P",
+	"81": "Q",
+	"82": "R",
+	"83": "S",
+	"84": "T",
+	"85": "U",
+	"86": "V",
+	"87": "W",
+	"88": "X",
+	"89": "Y",
+	"90": "Z",
+	"96": "NumPad0",
+	"97": "NumPad1",
+	"98": "NumPad2",
+	"99": "NumPad3",
+	"100": "NumPad4",
+	"101": "NumPad5",
+	"102": "NumPad6",
+	"103": "NumPad7",
+	"104": "NumPad8",
+	"105": "NumPad9",
+	/*"106" : KP_Multiply KP_Multiply 
+	"107" : KP_Add KP_Add 
+	"108" : KP_Separator KP_Separator 
+	"109" : KP_Subtract KP_Subtract 
+	"110" : KP_Decimal KP_Decimal 
+	"111" : KP_Divide KP_Divide */
+	"112": "F1",
+	"113": "F2",
+	"114": "F3",
+	"115": "F4",
+	"116": "F5",
+	"117": "F6",
+	"118": "F7",
+	"119": "F8",
+	"120": "F9",
+	"121": "F10",
+	"122": "F11",
+	"123": "F12",
+	/*"124" : F13 
+	"125" : F14 
+	"126" : F15 
+	"127" : F16 
+	"128" : F17 
+	"129" : F18 
+	"130" : F19 
+	"131" : F20 
+	"132" : F21 
+	"133" : F22 
+	"134" : F23 
+	"135" : F24 */
+	"144": "NumLock",
+	"145": "Scroll",		
+	"188" : ",",		
+	"190" : ".", 
+	"191" : "/",
+	"59" : ";",
+	"222" : "'",
+	"220" : "\\",
+	"219" : "[",
+	"221" : "]",
+	"173" : "-",
+	"61" : "=",
+	"107" : "+",
+	"109" : "-",
+	"111" : "/",
+	"106" : "*",
+	"110" : ".",
+	"192" : "~"
+		/*"187" : acute grave 
+		"189" : minus underscore 
+		"192" : numbersign apostrophe 
+		"210" : plusminus hyphen macron 
+		"211" : 
+		"212" : copyright registered 
+		"213" : guillemotleft guillemotright 
+		"214" : masculine ordfeminine 
+		"215" : ae AE 
+		"216" : cent yen 
+		"217" : questiondown exclamdown 
+		"218" : onequarter onehalf threequarters 
+		"220" : less greater bar 
+		"221" : plus asterisk asciitilde 
+		"227" : multiply division 
+		"228" : acircumflex Acircumflex 
+		"229" : ecircumflex Ecircumflex 
+		"230" : icircumflex Icircumflex 
+		"231" : ocircumflex Ocircumflex 
+		"232" : ucircumflex Ucircumflex 
+		"233" : ntilde Ntilde 
+		"234" : yacute Yacute 
+		"235" : oslash Ooblique 
+		"236" : aring Aring 
+		"237" : ccedilla Ccedilla 
+		"238" : thorn THORN 
+		"239" : eth ETH 
+		"240" : diaeresis cedilla currency 
+		"241" : agrave Agrave atilde Atilde 
+		"242" : egrave Egrave 
+		"243" : igrave Igrave 
+		"244" : ograve Ograve otilde Otilde 
+		"245" : ugrave Ugrave 
+		"246" : adiaeresis Adiaeresis 
+		"247" : ediaeresis Ediaeresis 
+		"248" : idiaeresis Idiaeresis 
+		"249" : odiaeresis Odiaeresis 
+		"250" : udiaeresis Udiaeresis 
+		"251" : ssharp question backslash 
+		"252" : asciicircum degree 
+		"253" : 3 sterling"
+		"254" : Mode_switch"*/
+}
